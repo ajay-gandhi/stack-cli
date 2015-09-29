f6d2cf2e8baf9d84b9a884549839b38e44098607
Ajay Gandhi <aag255@cornell.edu>
2015-09-29 11:46:17 -0400

    Add 'open in browser' option

--- app.js
+++ app.js
@@ -4,11 +4,13 @@
 var program  = require('commander'),
     inquirer = require('inquirer'),
     open     = require('open'),
-    readline = require('readline');
+    readline = require('readline'),
+    Promise  = require('es6-promise').Promise;
 
 // Local modules
 var stack_overflow = require('./lib/stackoverflow'),
-    printer        = require('./lib/printer');
+    printer        = require('./lib/printer'),
+    util           = require('./lib/util');
 
 // Formats the inputted tags for the StackOverflow API
 var so_format = function (str) {
@@ -32,49 +34,46 @@ if (!program.args.length) {
  * Wraps the question picker to keep asking if the user selects "More"
  */
 var pick_so_question = function () {
+  var q_data;
+
   return new Promise(function (resolve, reject) {
     stack_overflow
       .search_questions(program.args.join(' '), program.tags)
       .then(function (questions) {
+        q_data = questions;
 
         // Create answer choices
-        var answer_choices = questions.data.slice();
+        var answer_choices = q_data.data.slice();
         answer_choices.push(new inquirer.Separator());
 
-        if (questions.more) {
+        if (q_data.more) {
           answer_choices.push({
-            name: 'More',
+            name: 'More Questions',
             value: 'request_more_questions'
           });
           answer_choices.push(new inquirer.Separator());
         }
 
         // Create questions
-        var inq_questions = [{
-          type: 'list',
-          name: 'so_question',
-          message: 'Pick a question:',
-          choices: answer_choices
-        }];
+        return util.ask_list(answer_choices, 'Pick a question:');
+      })
+      .then(function (so_question) {
+        // User requested more questions
+        if (so_question === 'request_more_questions') {
+          pick_so_question.then(resolve);
 
-        inquirer.prompt(inq_questions, function (answers) {
-          // User requested more questions
-          if (answers.so_question === 'request_more_questions') {
-            pick_so_question.then(resolve);
+        // User picked a question
+        } else {
+          // Only want ids
+          var ids = q_data.data.map(function (q) {
+            return q.value;
+          });
 
-          // User picked a question
-          } else {
-            // Only want ids
-            var ids = questions.data.map(function (q) {
-              return q.value;
-            });
-
-            resolve({
-              all: ids,
-              main: answers.so_question
-            });
-          }
-        });
+          resolve({
+            all: ids,
+            main: so_question
+          });
+        }
       })
       .catch(function (e) {
         console.error(e);
@@ -82,7 +81,8 @@ var pick_so_question = function () {
   });
 }
 
-var user_q_id;
+var user_q_id,
+    user_q_url;
 
 // List the questions
 pick_so_question()
@@ -98,11 +98,35 @@ pick_so_question()
       return (q.id == user_q_id) ? q : acc;
     }, false);
 
+    user_q_url = user_q.link;
+
     printer.ruler();
     printer.title(user_q.title);
+    printer.ruler();
     printer.content(user_q.body);
 
-    return stack_overflow.get_answers(user_q_id);
+    // Ask for next command
+    var commands = [
+      {
+        name: 'View answers',
+        value: 'answers'
+      },
+      {
+        name: 'View in browser',
+        value: 'open'
+      }
+    ];
+    return util.ask_list(commands, 'What would you like to do?');
+  })
+  .then(function (command) {
+    // Open in browser
+    if (command === 'open') {
+      open(user_q_url);
+
+    // Fetch and display answers
+    } else if (command === 'answers') {
+      return stack_overflow.get_answers(user_q_id);
+    }
   })
   .then(function (answers) {
     var ans_choices = answers.map(function (a) {
@@ -112,14 +136,8 @@ pick_so_question()
       }
     });
 
-    var question = [{
-      type: 'list',
-      name: 'which_answer',
-      message: 'Choose an answer:',
-      choices: ans_choices
-    }];
-
-    inquirer.prompt(question, function (answers) {
-      printer.content(answers.which_answer);
-    });
+    return util.ask_list(ans_choices, 'Choose an answer:');
+  })
+  .then(function (answer_content) {
+    printer.content(answer_content);
   });
